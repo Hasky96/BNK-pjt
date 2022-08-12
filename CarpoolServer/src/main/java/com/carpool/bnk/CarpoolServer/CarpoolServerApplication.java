5ab2314d0a067ae157e8ca55a14a8fa28ac90c50
Haseok Jang <dean@HaseokJangs-MacBook-Air.local>
2022-08-12 10:19:43 +0900

    pjt setting

---
+++ CarpoolServer/src/main/java/com/carpool/bnk/CarpoolServer/CarpoolServerApplication.java
@@ -0,0 +1,13 @@
+package com.carpool.bnk.CarpoolServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolServerApplication.class, args);
+	}
+
+}
---
+++ CarpoolServer/src/test/java/com/carpool/bnk/CarpoolServer/CarpoolServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpool.bnk.CarpoolServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
