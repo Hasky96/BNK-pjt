1e6644441f1ca08442933d42d8282236ceaf3028
Haseok Jang <dean@HaseokJangs-MacBook-Air.local>
2022-08-16 13:16:16 +0900

    User processing

--- CarpoolServer/src/main/java/com/carpool/bnk/CarpoolServer/domain/user/controller/UserController.java
+++ CarpoolServer/src/main/java/com/carpool/bnk/CarpoolServer/domain/user/controller/UserController.java
@@ -1,15 +1,24 @@
 package com.carpool.bnk.CarpoolServer.domain.user.controller;
 
+import com.carpool.bnk.CarpoolServer.domain.user.request.UserSignupReq;
+import com.carpool.bnk.CarpoolServer.domain.user.response.UserSignupRes;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-// @RequestMapping("/api/user")
+@RequestMapping("/api/user")
 public class UserController {
+
     @GetMapping("/test")
     public ResponseEntity<String> test(){
         return ResponseEntity.status(200).body("Hi!");
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserSignupRes> signup(@RequestBody UserSignupReq body){
+
+
+
+        return ResponseEntity.status(200).body(new UserSignupRes());
     }
 }
