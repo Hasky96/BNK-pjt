1a30707c096d4adee5a5e3b13dc52530eb36a8ac
Jung2022 <97584518+Jung2022@users.noreply.github.com>
2022-08-16 15:46:30 +0900

    login 만드는 중

---
+++ CarpoolApp/app/src/main/java/com/example/carpoolapp/ui/user/LoginFragment.java
@@ -0,0 +1,25 @@
+package com.example.carpoolapp.ui.user;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.carpoolapp.R;
+import com.example.carpoolapp.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+    private FragmentLoginBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding=FragmentLoginBinding.inflate(getLayoutInflater());
+
+
+        return binding.getRoot();
+    }
+}
