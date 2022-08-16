aa062d32f8cdbfe9a142b6375826f45ed6eec6db
yejin087 <zinyizhen8@gmail.com>
2022-08-16 16:22:21 +0900

    add 카풀 dto

--- CarpoolApp/app/src/main/java/com/example/carpoolapp/ui/carpool/CarpoolRegisterActivity.java
+++ CarpoolApp/app/src/main/java/com/example/carpoolapp/ui/carpool/CarpoolRegisterActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -19,6 +20,8 @@ public class CarpoolRegisterActivity extends AppCompatActivity {
     private ArrayAdapter arrayAdapter;
     int flag;
     TextView tvDepartureTime;
+    Button btnCarpoolRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +30,10 @@ public class CarpoolRegisterActivity extends AppCompatActivity {
 
         spPersonamount = findViewById(R.id.spPersonamount);
         tvDepartureTime = findViewById(R.id.tvDepartureTime);
+        btnCarpoolRegister = findViewById(R.id.btnCarpoolRegister);
 
         arrayAdapter = ArrayAdapter.createFromResource(this, R.array.spPersonamount, android.R.layout.simple_spinner_item);
         spPersonamount.setAdapter(arrayAdapter);
-
 
         TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
 
@@ -55,6 +58,13 @@ public class CarpoolRegisterActivity extends AppCompatActivity {
                 dialog.setTitle("시간");
                 dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
                 dialog.show();
+            }
+        });
+
+        btnCarpoolRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
 
