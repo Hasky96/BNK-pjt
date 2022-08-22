a36d6ba1eff5dfa06158662dc036b36f39e568f9
Jung2022 <97584518+Jung2022@users.noreply.github.com>
2022-08-22 11:12:22 +0900

    임시

--- CarpoolApp/app/src/main/java/com/example/carpoolapp/ui/carpool/CarpoolFragment.java
+++ CarpoolApp/app/src/main/java/com/example/carpoolapp/ui/carpool/CarpoolFragment.java
@@ -5,25 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.carpoolapp.R;
 import com.example.carpoolapp.databinding.FragmentCarpoolBinding;
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 // 카풀 리스트 프래그먼트
 public class CarpoolFragment extends Fragment {
 
 	private FragmentCarpoolBinding binding;
-	ExtendedFloatingActionButton flbCarpoolRegister;
-	RecyclerView rvCarpool;
 	CarpoolViewModel carpoolViewModel;
 	CarpoolAdapter carpoolAdapter;
 
