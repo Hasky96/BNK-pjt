2b19713aae35d983b79675d5e9b80c32960fefee
Jung2022 <97584518+Jung2022@users.noreply.github.com>
2022-08-23 16:51:39 +0900

    지도 개선, 스플래시 애니메이션 추가

--- CarpoolApp/app/src/main/java/com/example/carpoolapp/ui/splash/SplashFragment.java
+++ CarpoolApp/app/src/main/java/com/example/carpoolapp/ui/splash/SplashFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
@@ -46,9 +47,13 @@ public class SplashFragment extends Fragment {
             public void run() {
                 if (Authorization != null) {
                     Log.d("jjk", Authorization);
-                    Intent intent = new Intent(getActivity(), MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(intent);
+                    if (getActivity()!=null){
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intent);
+                    }else {
+                        
+                    }
                 } else {
                     //딜레이 후 시작할 코드 작성
                     //데이터를 넘겨줘야할 때 사용하기
