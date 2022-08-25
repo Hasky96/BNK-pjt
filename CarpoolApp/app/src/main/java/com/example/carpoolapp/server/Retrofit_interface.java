f22f506fb530195c174b45df966360beee32a81b
Jung2022 <97584518+Jung2022@users.noreply.github.com>
2022-08-25 17:49:32 +0900

    id체크

--- CarpoolApp/app/src/main/java/com/example/carpoolapp/server/Retrofit_interface.java
+++ CarpoolApp/app/src/main/java/com/example/carpoolapp/server/Retrofit_interface.java
@@ -75,6 +75,6 @@ public interface Retrofit_interface {
 	@POST("/api/carpool/{carpoolNo}/comment")
 	Call<CommonResponse> registerComment(@Header("Authorization") String Authorization,@Path("carpoolNo") int carpoolNo,@Body CommentRequest commentRequest);
 
-	@GET("/api/user/idcheck?id={checkId}")
-	Call<CommonResponse> idCheck(@Path("checkId")String userId);
+	@GET("/api/user/idcheck")
+	Call<CommonResponse> idCheck(@Query("id") String userId);
 }
--- CarpoolApp/app/src/main/java/com/example/carpoolapp/ui/user/SignupFragment.java
+++ CarpoolApp/app/src/main/java/com/example/carpoolapp/ui/user/SignupFragment.java
@@ -1,11 +1,14 @@
 package com.example.carpoolapp.ui.user;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,54 +39,78 @@ public class SignupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding=FragmentSignupBinding.inflate(getLayoutInflater());
+        binding = FragmentSignupBinding.inflate(getLayoutInflater());
 
         //더 간단한 방법이 있긴한데 엔터를 안치고 로그인버튼 누르면 값 저장이 안됨.
         binding.eTSignupId.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
-            public void afterTextChanged(Editable editable) {userId=editable.toString();}
+            public void afterTextChanged(Editable editable) {
+                userId = editable.toString();
+            }
         });
         binding.eTSignupPw.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
-            public void afterTextChanged(Editable editable) {userPw=editable.toString();}
+            public void afterTextChanged(Editable editable) {
+                userPw = editable.toString();
+            }
         });
         binding.eTSignupCarNo.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
-            public void afterTextChanged(Editable editable) {userCarNo=editable.toString();}
+            public void afterTextChanged(Editable editable) {
+                userCarNo = editable.toString();
+            }
         });
         binding.eTSignupCarInfo.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
-            public void afterTextChanged(Editable editable) {userCarInfo=editable.toString();}
+            public void afterTextChanged(Editable editable) {
+                userCarInfo = editable.toString();
+            }
         });
 
         binding.buttonSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SignupRequest signupRequest=new SignupRequest(userId,userPw,userCarInfo,userCarNo);
-                call= Retrofit_client.getApiService().signup(signupRequest);
+                SignupRequest signupRequest = new SignupRequest(userId, userPw, userCarInfo, userCarNo);
+                call = Retrofit_client.getApiService().signup(signupRequest);
                 call.enqueue(new Callback<SignupResponse>() {
                     @Override
                     public void onResponse(Call<SignupResponse> call, Response<SignupResponse> response) {
                         Toast.makeText(getActivity().getApplicationContext(), "회원가입 성공", Toast.LENGTH_SHORT).show();
                         //데이터를 넘겨줘야할 때 사용하기
-                        LoginFragment fragment=new LoginFragment();
-                        Bundle bundle=new Bundle();
+                        LoginFragment fragment = new LoginFragment();
+                        Bundle bundle = new Bundle();
                         fragment.setArguments(bundle);
                         //프래그먼트에서 프래그먼트로 이동
                         requireActivity()
@@ -93,6 +120,7 @@ public class SignupFragment extends Fragment {
                                 .setCustomAnimations(R.anim.slide_page_in, R.anim.slide_page_out)
                                 .replace(R.id.ConstraintFragment, fragment).commit();
                     }
+
                     @Override
                     public void onFailure(Call<SignupResponse> call, Throwable t) {
                         Toast.makeText(getActivity().getApplicationContext(), "회원가입 실패", Toast.LENGTH_SHORT).show();
@@ -102,24 +130,47 @@ public class SignupFragment extends Fragment {
             }
         });
 
+
         binding.idCheckButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                call_idCheck=Retrofit_client.getApiService().idCheck(userId);
-                call_idCheck.enqueue(new Callback<CommonResponse>() {
-                    @Override
-                    public void onResponse(Call<CommonResponse> call, Response<CommonResponse> response) {
-                        
-                    }
-
-                    @Override
-                    public void onFailure(Call<CommonResponse> call, Throwable t) {
-
-                    }
-                });
+                if (userId != null) {
+                    call_idCheck = Retrofit_client.getApiService().idCheck(userId);
+                    call_idCheck.enqueue(new Callback<CommonResponse>() {
+                        @Override
+                        public void onResponse(Call<CommonResponse> call, Response<CommonResponse> response) {
+                            if (response.code()==200) {
+                                showDialog("사용 가능한 아이디입니다.");
+                            } else if(response.code()==400){
+                                showDialog("이미 있는 아이디 입니다.");
+                            }else {
+                                showDialog("사용 불가능한 아이디 입니다.");
+                            }
+                        }
+                        @Override
+                        public void onFailure(Call<CommonResponse> call, Throwable t) {
+                            showDialog("통신 실패");
+                        }
+                    });
+                }else {
+                    showDialog("아이디를 입력하세요.");
+                }
             }
         });
 
+
         return binding.getRoot();
+    }
+
+    void showDialog(String text) {
+        AlertDialog.Builder msgBuilder = new AlertDialog.Builder(getActivity())
+                .setTitle(text)
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+        AlertDialog msgDlg = msgBuilder.create();
+        msgDlg.show();
     }
 }
